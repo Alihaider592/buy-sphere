ca3af4bf528341ed3159d4c2c653eb541f3f8494
Malik Ali <alicoder592@gmail.com>
2025-09-18 15:00:38 +0500

    redux presist removed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./store";
 import { ShopContextProvider } from "./components/shopcontaxt";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <ShopContextProvider>
-    <Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
+  <ShopContextProvider>
     <App />
-    </PersistGate>
-    </Provider>
   </ShopContextProvider>
 );
-
-
